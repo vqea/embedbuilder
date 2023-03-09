c5c65bd8544459d77e6230e68b74c6755487ccba
₿oss <110758135+vqea@users.noreply.github.com>
2023-03-08 22:35:07 -0500

    Create index.jsx

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className="copyright">Copyright &copy; 2019 - Vqea</div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
